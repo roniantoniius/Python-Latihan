1f0fe6ee72514124a8080d61360176237c119a5c
roniantonius <roniantoniuss@upi.edu>
2025-02-11 21:53:07 +0700

    Part 7 about Data preprocessing

--- TMC/mooc-programming-24/part07-15_who_cheated_2/src/who_cheated_2.py
+++ TMC/mooc-programming-24/part07-15_who_cheated_2/src/who_cheated_2.py
@@ -1,1 +1,60 @@
 # Write your solution here
+from datetime import timedelta
+def mulai() -> list:
+    daftarMulai = []
+    with open("src/start_times.csv", mode='r', encoding='utf-8') as mulai:
+        for baris in mulai:
+            nama, waktu = baris.strip().split(";")
+            jam, menit = map(int, waktu.split(":"))
+            sekarang = timedelta(hours=jam, minutes=menit)
+            daftarMulai.append({'name':nama, 'time': sekarang})
+    return daftarMulai
+
+def submisiNilai() -> list:
+    daftarAkhir = []
+    with open("src/submissions.csv", mode='r', encoding='utf-8') as start:
+        for daftar in start:
+            name, task, points, waktus = daftar.strip().split(";")
+            jams, menits = map(int, waktus.split(":"))
+            akhir = timedelta(hours=jams, minutes=menits)
+            daftarAkhir.append({'name': name, 'task': task, 'points': points, 'time': akhir})
+    return daftarAkhir
+
+def cheaters() -> list:
+    daftarMulai = mulai()
+    daftarAkhir = submisiNilai()
+
+    curang = set()
+    for i in daftarMulai:
+        for j in daftarAkhir:
+            if i["name"] == j["name"] and j["time"] - i["time"] > timedelta(hours=3):
+                curang.add(i["name"])
+    
+    return list(curang)
+
+def final_points() -> dict:
+    daftarMulai = mulai()
+    daftarAkhir = submisiNilai()
+    final = {}
+    for i in daftarMulai:
+        for j in daftarAkhir:
+            if i["name"] == j["name"] and j["time"] - i["time"] < timedelta(hours=3):
+                submisi = int(j["task"])
+                nilai = int(j["points"])
+                if 1 <= submisi <= 8 and 0 <= nilai <= 6:
+                    if i["name"] not in final:
+                        final[i["name"]] = {}
+                    if submisi in final[i["name"]]:
+                        final[i["name"]][submisi] = max(final[i["name"]][submisi], nilai)
+                    else:
+                        final[i["name"]][submisi] = nilai
+
+    baru = {}
+    for key, value in final.items():
+        jumlah = 0
+        for k, v in value.items():
+            jumlah += v
+        baru[key] = jumlah
+    return baru
+if __name__ == "__main__":
+    print(final_points())
--- TMC/mooc-programming-24/part07-16_spellchecker_2/src/spellchecker_2.py
+++ TMC/mooc-programming-24/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,20 @@
 # Write your solution here
+from difflib import get_close_matches
+
+user = input("Write text: ")
+
+with open('src/wordlist.txt') as file:
+    word_list = []
+    dafar = []
+    for line in file:
+        word_list.append(line.strip())
+    for word in user.split(' '):
+        if word.lower() in word_list:
+            print(word, end=' ')
+        else:
+            dafar.append(word)
+            print(f'*{word}*', end=' ')
+    print("\nsuggestions:")
+    for kata in dafar:
+        rekomendasi = get_close_matches(kata, word_list)
+        print(f"{kata}: {', '.join(rekomendasi)}")
