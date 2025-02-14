a9a1bb18498f595096a81530d5ca0cd89bed14ec
roniantonius <roniantoniuss@upi.edu>
2025-02-14 22:21:47 +0700

    Part 7 Done but the last exercise about new language got an error when using location command with value start:, damn we got 50% point

--- TMC/mooc-programming-24/part07-17_string_helper/src/string_helper.py
+++ TMC/mooc-programming-24/part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,29 @@
 # Write your solution here
+import string
+def change_case(orig_string: str) -> str:
+    kata = ""
+    for huruf in orig_string:
+        if huruf.islower():
+            kata += huruf.upper()
+        else:
+            kata += huruf.lower()
+    return kata
+
+def split_in_half(orig_string: str) -> tuple:
+    awal = ""
+    akhir = ""
+    panjang = len(orig_string)
+    tengah = panjang // 2
+    for i in range(panjang):
+        if i < tengah:
+            awal += orig_string[i]
+        else:
+            akhir += orig_string[i]
+    return awal, akhir
+
+def remove_special_characters(orig_string: str) -> str:
+    huruf = ""
+    for kata in orig_string:
+        if kata.isalnum() or kata.isspace():
+            huruf += kata
+    return huruf
--- TMC/mooc-programming-24/part07-18_own_programming_language/src/own_language.py
+++ TMC/mooc-programming-24/part07-18_own_programming_language/src/own_language.py
@@ -1,1 +1,68 @@
 # Write your solution here
+from string import ascii_uppercase
+def run(sistem: str) -> list:
+    daftarVariabel = dict.fromkeys(ascii_uppercase, 0)
+    cetak = []
+    lokasi = {}
+
+    for i, v in enumerate(sistem):
+        if v.endswith(":"):
+            lokasi[baris[:-1]] = i
+
+    i = 0
+    while i < len(sistem):
+        baris = sistem[i].split()
+        if baris[0].endswith(":"):
+            i += 1
+            continue
+        elif baris[0] == "END":
+            break
+        elif baris[0] == "PRINT":
+            cetak.append(daftarVariabel[baris[1]])
+        elif baris[0] == "MOV":
+            daftarVariabel[baris[1]] = int(baris[2]) if baris[2].isdigit() else daftarVariabel[baris[2]]
+        elif baris[0] == "ADD":
+            daftarVariabel[baris[1]] += int(baris[2]) if baris[2].isdigit() else daftarVariabel[baris[2]]
+        elif baris[0] == "SUB":
+            daftarVariabel[baris[1]] -= int(baris[2]) if baris[2].isdigit() else daftarVariabel[baris[2]]
+        elif baris[0] == "MUL":
+            daftarVariabel[baris[1]] *= int(baris[2]) if baris[2].isdigit() else daftarVariabel[baris[2]]
+        elif baris[0] == "JUMP":
+            if baris[1] in lokasi:
+                i = lokasi[baris[1]]
+                continue
+        elif baris[0] == "IF":
+            operator = baris[2]
+            awal = int(baris[1] if baris[1].isdigit() else daftarVariabel[baris[1]])
+            akhir = int(baris[3] if baris[3].isdigit() else daftarVariabel[baris[3]])
+            kondisi = {
+                "==": awal == akhir,
+                "!=": awal != akhir,
+                "<": awal < akhir,
+                ">": awal > akhir,
+                "<=": awal <= akhir,
+                ">=": awal >= akhir,
+            }
+            if kondisi[operator] and baris[4] == "JUMP" and baris[5] in lokasi:
+                i = lokasi[baris[5]]
+                continue
+        i += 1
+
+    return cetak
+
+if __name__ == '__main__':
+    program1 = []
+    program1.append("MOV A 10")
+    program1.append("MOV B 2")
+    program1.append("PRINT A")
+    program1.append("PRINT B")
+    program1.append("ADD A B")
+    program1.append("PRINT A")
+    program1.append("END")
+    program1.append("MOV B A")
+    program1.append("SUB B 8")
+    program1.append("PRINT B")
+    program1.append("SUB A B")
+    program1.append("PRINT A")
+    result = run(program1)
+    print(result)
